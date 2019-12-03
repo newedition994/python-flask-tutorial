cdb9cd7d3bd4641d41379ea9b87f9407d382aaf8
Breyanna Vass <breyannavass@gmail.com>
2019-12-02 22:11:44 -0500

    adding in the get posts method

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method == 'GET':
         pass
-    
+
     if request.method == 'POST':
         name = request.form.get('name')
         post = request.form.get('post')
         create_post(name, post)
 
-    return render_template('index.html')
+    posts = get_posts()
+
+    return render_template('index.html', posts=posts)
