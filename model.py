b1a166e204228c0d19bcfc564994ebb2561f5583
Breyanna Vass <breyannavass@gmail.com>
2019-12-02 22:07:59 -0500

    pulls in the data for the posts

--- model.py
+++ model.py
@@ -2,7 +2,18 @@ import sqlite3
 
 ROOT = path.dirname(path.relpath((__file__)))
 
+
 def create_post(name, content):
     con = sql.connect(path.join(ROOT, 'database.db'))
     cur = con.cursor()
     cur.excute('insert into post (name, content) values(?, ?)', (name, content))
+    con.commit()
+    con.close()
+
+
+def get_posts():
+    con = sql.connect(path.join(ROOT, 'database.db'))
+    cur = con.cursor()
+    cur.excute('select * from posts')
+    posts = cur.fetchall()
+    return posts
